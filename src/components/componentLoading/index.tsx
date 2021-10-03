6456041a38bfef9f9f8c792eb114af5522eafe35
hoanghuychh <hoanghuychh@gmail.com>
2021-10-03 12:12:05 +0700

    Add status succes for recevie feature

---
+++ src/components/componentLoading/index.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import stylesSheet from './styles';
+
+function Loading({isLoading}) {
+  console.log('chh_log ---> isLoading', isLoading);
+  if (isLoading)
+    return (
+      <View style={stylesSheet.loading}>
+        <ActivityIndicator size="large" color="#ec4427" />
+      </View>
+    );
+  return null;
+}
+export default memo(Loading);
---
+++ src/components/componentLoading/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const stylesSheet = StyleSheet.create({
+  loading: {
+    position: 'absolute',
+    top: '25%',
+    elevation: 100,
+  },
+});
+
+export default stylesSheet;
